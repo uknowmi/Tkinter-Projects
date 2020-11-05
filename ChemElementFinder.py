e07bd5f9f4542b6317b34288a1a9076a53c28aee
uknowmi <58881954+uknowmi@users.noreply.github.com>
2020-11-05 11:00:07 +0530

    Changed the name of the file

---
+++ ChemElementFinder.py
@@ -0,0 +1,22 @@
+# Importing required libraries here
+from tkinter import *  # For importing everything from tkinter to make a GUI interface
+
+from PIL import ImageTk, Image # For showing images with Tkinter
+
+from tkinter import messagebox #For showing popup messages on errors
+
+# _____________________________________________________________
+
+# Making the User interface
+
+root = Tk()#making the main window for user interaction
+
+root.title('ChemElement Finder')#Setting title of the window
+
+root.geometry('1355x825')#To fix the size and geometry of the window
+
+root.iconbitmap('flaskicon.ico')#Adding a flask shaped icon to the window
+
+print('Hello')
+
+mainloop()
